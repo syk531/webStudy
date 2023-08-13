a7629ddb744e63f61bc432b8b4276ddaa2c905bd
송영광(FBI00269) 파이언넷 <syk531@pionnet.co.kr>
2023-08-14 08:52:57 +0900

    프로젝트 이름 변경

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String args[]){
+    public static void main(String[] args){
         User user = User.builder()
                         .name("test ")
                         .phone("01011112222")
